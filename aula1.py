fc845e1d3dabee207e625c8052c08b57e7697416
Evandro Zaninho <ezaninho@gmail.com>
2026-02-06 15:25:38 -0300

    Create aula1.py

---
+++ aula1.py
@@ -0,0 +1,25 @@
+nome_prof = "Ana"
+idade_prof = int(input("Digite sua idade: "))
+altura_prof = 1.61
+
+print("O nome da professora é", nome_prof)
+
+altura_desejada = round(altura_prof + 0.04, 2)
+
+print("A professora queria ter", altura_desejada, "de altura")
+#altura_desejada:.2f
+print("A idade da professora é", idade_prof)
+
+#adição +
+#subtração -
+#multiplicação *
+#divisão /
+
+#Calculadora simples
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+print("Soma: ", numero1 + numero2)
+print("Subtração:", numero1 - numero2) 
+print("Multiplicação:", numero1 * numero2) 
+print("Divisão:", numero1 / numero2)
