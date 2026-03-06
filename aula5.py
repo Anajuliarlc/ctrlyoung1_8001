3ef5170bef71e7efe83327a72d08c1f144cd3509
Ana Júlia <anajuliaritalc@gmail.com>
2026-03-06 15:27:06 -0300

    Update aula5.py

--- aula5.py
+++ aula5.py
@@ -1,6 +1,32 @@
-numero = int(input("Digite um numero: "))
+"""numero = int(input("Digite um numero: "))
 
 if numero > 10:
     print("Maior que 10")
+elif numero == 10:
+    print("igual 10")
 else: 
-    print("menor ou igual a 10")
+    print("menor que 10")
+
+idade = int(input("Digite uma idade: "))
+
+if idade >= 18:
+    print("maior que 18")
+else:
+    print("menor")
+
+numero = int(input("Digite um numero: "))
+
+if numero % 3 == 0 and numero % 5 == 0:
+    print("multiplo dos dois")
+else:
+    print("não é")
+
+for numero in range(0,11,2):
+    print(numero)"""
+
+numero = int(input("Digite um numero: "))
+contador = 0
+
+while contador <= numero:
+    print(contador)
+    contador += 1
